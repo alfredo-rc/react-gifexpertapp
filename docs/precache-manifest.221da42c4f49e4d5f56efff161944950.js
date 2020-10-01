5aa1fe3442d85e97bf907be853d38ca1cc16c283
Alfredo Robles <luis.robles.c18@gmail.com>
2020-10-01 11:39:11 -0500

    Docs Agregado

---
+++ docs/precache-manifest.221da42c4f49e4d5f56efff161944950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e079ee6c299b5c81f8d0ce6562440ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1aa874a13ce6db29b201",
+    "url": "/static/css/main.1be731e3.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa874a13ce6db29b201",
+    "url": "/static/js/main.67957433.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
